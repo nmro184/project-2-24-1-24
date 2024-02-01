2b3ce0a5f04a3126d94a90643115ef44c2445ccc
nmro184 <nimrod.balaban@gmail.com>
2024-02-01 20:06:24 +0200

    implemented users and update

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
-from flask import Flask , render_template , request
-from db import new_task, get_tasks , delete_task
+from flask import Flask , render_template , request , session , redirect
+from db import new_task, get_tasks , delete_task , update_task
 app = Flask(__name__)
 
+app.secret_key = "kjdbkjsdbkjjsldjdsbn"
 @app.route('/')
 def login():
     return render_template("login.html")
 
-@app.route('/home' , methods = ['POST' , 'GET'])
-def home():
-    return render_template("home.html" , tasks = get_tasks())
+@app.route('/home/<username>' , methods = ['POST' , 'GET'])
+def home(username):
+    if username not in session:
+        session[username] = username
+    return render_template("home.html" , tasks = get_tasks(username) , username = username)
 
-@app.route('/create')
-def create():
-    new_task(description = request.args['description'] , date = request.args['date'] , category =request.args['category'])
-    return home()
+@app.route('/create/<username>' , methods = ['POST' , 'GET'])
+def create(username):
+    new_task(description = request.form['description'] , date = request.form['date'] , category =request.form['category'] , username = username)
+    return redirect (f"/home/{username}")
 
-@app.route('/delete' , methods = ['POST' , 'GET'])
-def delete():
-    delete_task(request.form['deleteid'])
-    return home()
+@app.route('/delete/<username>' , methods = ['POST'])
+def delete(username):
+    delete_task(request.form['deleteid'] , username)
+    return redirect(f'/home/{username}')
+
+@app.route('/update/<username>' , methods = ['POST'])
+def update(username):
+    update_task(new_description= request.form['description'] , new_date= request.form['date'] , new_category= request.form['category'] , updateid= request.form['updateid'] , username = username)
+    return redirect(f'/home/{username}')
--- db.py
+++ db.py
@@ -7,14 +7,23 @@ def query(sql: str = "", data: tuple = ()):
         cur = conn.cursor()
         return cur.execute(sql, data).fetchall()
 
-def new_task(description, date , category):
-    data = (description , date  , category)
-    query("INSERT INTO tasks (description, date , category) VALUES (?, ? ,?)", data)
+def new_task(description, date , category , username):
+    data = (description , date  , category , username)
+    query("INSERT INTO tasks (description, date , category , username) VALUES (?, ? ,? ,?)", data)
 
-def get_tasks():
-    task_tuple_list = query("SELECT * FROM tasks ")
+def get_tasks(username):
+    if username == "admin":
+        task_tuple_list = query("SELECT * FROM tasks ")
+    else:
+        task_tuple_list = query(f"SELECT * FROM tasks WHERE username ='{username}'")
     return [Task(task) for task in task_tuple_list]
 
-def delete_task(id):
-    query(f"DELETE from tasks WHERE id ={id}")
+def delete_task(id , username):
+  if username == "admin":
+      query(f"DELETE FROM tasks WHERE id = {id} ")
+  data = (id,username)
+  query("DELETE FROM tasks WHERE id = ? AND username = ?", data)
 
+def update_task(new_description , new_date , new_category  , updateid , username):
+    data = (new_description , new_date, new_category, updateid , username )
+    query("UPDATE tasks SET description = ?, date = ?, category = ? WHERE id = ? AND username = ?" , data)
